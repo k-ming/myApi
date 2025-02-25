a849289207aaacdc5c285361de6aaecc6c5ecb2b
zh88168@yeah.net <zh88168@yeah.net>
2025-02-25 23:30:14 +0800

    fist push

---
+++ main.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jan 15 19:57:49 2025
+@author: hb32366
+"""
+
+from fastapi import FastAPI
+import pydantic
+from enum import Enum
+
+app = FastAPI()
+
+@app.get("/")
+async def root():
+    return {"msg":"hello fastapi"}
+
+# 路径参数--并指定参数类型
+@app.get("/items/{item_id}")
+async def read_item(item_id: int):
+    return {'item_id':item_id}
+
+
+# 枚举
+class ModelName(str, Enum):
+    alexnet = 'alexnet'
+    resnet = 'resnet'
+    lenet = 'lenet'
+
+@app.get("/models/{model_name}")
+async def get_model(model_name: ModelName):
+    if model_name is ModelName.alexnet:
+        return {"model_name": model_name, 'msg': "Deep Learning FTW!"}
+    elif model_name is ModelName.resnet:
+        return {"model_name": model_name, "msg": "LeCNN all the images"}
+    
+    return {"model_name": model_name, "msg":"Have some residuals"}
