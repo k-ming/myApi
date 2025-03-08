657baa4c63dc4aa3fa16e1c01173d8a5526565c6
zh88168@yeah.net <zh88168@yeah.net>
2025-03-09 00:09:28 +0800

    ignore .ideal folder

--- dependencies.py
+++ dependencies.py
@@ -1,9 +1,9 @@
 from fastapi import Header, HTTPException
 
 async def get_token_header(token: str = Header(...)):
-    if token != "fake-super-secret-token":
+    if token != "X-Token":
         raise HTTPException(status_code=400, detail="X-Token header invalid")
 
 async def get_query_token(token: str):
-    if token != "jessica":
+    if token != "Jessica":
         raise HTTPException(status_code=400, detail="No Jessica token provided")
---
+++ src/searchModle.py
@@ -0,0 +1,29 @@
+'''
+查询参数模型
+'''
+from typing import Annotated, Literal
+from fastapi import APIRouter, Depends, Query
+from ..dependencies import get_token_header
+from pydantic import BaseModel, Field
+
+router = APIRouter(
+    prefix="/search_model",
+    tags=["search_model"],
+    responses={404: {"description": "Not found"}},
+    dependencies=[Depends(get_token_header)],
+)
+
+class FilterParams(BaseModel):
+    limit: int = Field(100, gt=0, le=100)
+    offset: int = Field(0, gt=0)
+    order_by: Literal["created_at", "updated_at"] = "created_at" # 检查特殊类型注解
+    tags: list[str] = []
+    model_config = {"extra": "forbid"} # 禁止额外的参数
+
+@router.get('/items')
+async def read_items(filter_query: Annotated[FilterParams, Query()] ):
+    """
+    :param filter_query: 特殊类型注解 Annotated [T, x]
+    :return:
+    """
+    return filter_query
